d5885dc04abc16d0bbedadcc521cb203a43391a9
fibin <fibinkunnath@gmail.com>
2025-12-31 10:05:10 +0530

    Time: 4 ms (89.88%), Space: 64.1 MB (54.02%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+   if( x < 0) return false;
+
+   let ori = x ;
+   let rev = 0 ;
+   while( x > 0 ) {
+    rev = rev*10 + (x%10)
+    x=Math.floor(x/10)
+   }
+   return ori===rev
+
+};
