0fcc4bd05eb7c92297b1d12b69028154c65f9941
fibin <fibinkunnath@gmail.com>
2026-02-03 10:47:44 +0530

    Time: 0 ms (100%), Space: 56.1 MB (61.29%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function isTrionic(nums) {
+    const n = nums.length;
+    if (n < 4) return false;
+
+    let i = 0;
+
+   
+    while (i + 1 < n && nums[i] < nums[i + 1]) {
+        i++;
+    }
+    if (i === 0) return false; 
+
+    while (i + 1 < n && nums[i] > nums[i + 1]) {
+        i++;
+    }
+    if (i === n - 1) return false; 
+
+    while (i + 1 < n && nums[i] < nums[i + 1]) {
+        i++;
+    }
+
+    return i === n - 1;
+}
